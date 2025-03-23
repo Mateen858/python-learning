5fd13764b8f35557111b87b6d29297ba77b640f8
DESKTOP-K93KVM8\Rana Mateen <a.mateen858@hotmail.com>
2025-03-23 22:24:23 +0500

    first

---
+++ data_types.py
@@ -0,0 +1,23 @@
+from math import *
+name = "john"
+age = "20"
+phrase = "This is a test phrase"
+liters = 12.5
+test = [1,2,3,5,6,8,9]
+print("He was a man named " + name)
+print("He was" + age + "years old")
+print(phrase.upper())
+print(len(phrase))
+print(phrase.index("T"))
+print(phrase.replace("This", "He"))
+print(liters)
+print(str(liters) + "milk")
+print(pow(3,2))
+print(max(test))
+print(floor(3.7))
+print(ceil(3.7))
+print(sqrt(6))
+
+#how to take input from a user
+user_name =  input("Please enter your name: ")
+print("hi " + user_name)
